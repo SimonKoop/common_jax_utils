59e81e0e947b1de54468d64fe29bb3ffcc29e40f
simon <s.m.koop@tue.nl>
2025-09-17 15:11:38 +0200

    fixed compatibility issue with newer jax versions

--- src/common_jax_utils/debug_utils.py
+++ src/common_jax_utils/debug_utils.py
@@ -4,7 +4,7 @@ def summary(obj):
     """ 
     Give a more readable summary of a pytree. 
     """
-    return str(jax.tree_map(
+    return str(jax.tree_util.tree_map(
         lambda x: f"array{x.shape}" if hasattr(x, 'shape') else repr(x),
         obj
     ))
